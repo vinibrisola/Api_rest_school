557187bd3d4f0326e406ee558121ca3fb7440824
vinicius brisola <vinibrisola2@gmail.com>
2023-01-26 12:49:49 -0300

    configurando e testando o Sequelize

--- src/controllers/HomeControllers.js
+++ src/controllers/HomeControllers.js
@@ -1,8 +1,18 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req,res){
-    res.json({
-      tudoCerto:true,
+  async index(req,res){
+    const novoAluno = await Aluno.create({
+      nome: 'vinicius',
+      sobrenome: 'brisola alves',
+      email: 'vinicius@gmail.com',
+      idade: 23,
+      peso: 84,
+      altura: 2.5,
     })
+
+
+    res.json(novoAluno);
   }
 }
 
