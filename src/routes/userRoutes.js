a53d01d6c2a5c4ae6fb5852eee9b85b4b6c28841
vinicius brisola <vinibrisola2@gmail.com>
2023-02-02 13:00:20 -0300

    crud usuarios

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import userControllers from "../controllers/UserControllers";
 const router = new Router();
 
 router.post('/',userControllers.store);
+router.get('/',userControllers.index);
+router.get('/:id',userControllers.show);
+router.put('/:id',userControllers.update);
+router.delete('/:id',userControllers.delete);
 
 export default router;
